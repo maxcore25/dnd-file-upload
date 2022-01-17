0c9b144a375843415d61b2b7c69bbb5d4ad72053
maxcore25 <mkorobov2002@mail.ru>
2022-01-17 09:26:00 +0300

    Drop Zone stylized and Drop Event created

---
+++ main.js
@@ -0,0 +1,37 @@
+document.querySelectorAll('.drop-zone__input').forEach(inputElement => {
+  const dropZoneElement = inputElement.closest('.drop-zone');
+
+  dropZoneElement.addEventListener('dragover', e => {
+    e.preventDefault();
+    dropZoneElement.classList.add('drop-zone--over');
+  });
+
+  ['dragleave', 'dragend'].forEach(type => {
+    dropZoneElement.addEventListener(type, e => {
+      dropZoneElement.classList.remove('drop-zone--over');
+    });
+  });
+
+  dropZoneElement.addEventListener('drop', e => {
+    e.preventDefault();
+
+    if (e.dataTransfer.files.length) {
+      inputElement.files = e.dataTransfer.files;
+      console.log(inputElement.files);
+      updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+    }
+
+    dropZoneElement.classList.remove('drop-zone--over');
+  });
+});
+
+function updateThumbnail(dropZoneElement, file) {
+  const thumbnailElement = dropZoneElement.querySelector('.drop-zone__thumb');
+
+  // First time there's no thumbnail element, so lets create it
+  if (!thumbnailElement) {
+    thumbnailElement = document.createElement('div');
+    thumbnailElement.classList.add('drop-zone__thumb');
+    dropZoneElement.appendChild(thumbnailElement);
+  }
+}
